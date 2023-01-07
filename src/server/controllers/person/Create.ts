e720dc895a6c29a74400ba46fc0e57ab71d79435
Rafiere <rafapgbr@gmail.com>
2023-01-07 17:52:42 -0300

    feat: adding yup validations to endpoint

--- src/server/controllers/person/Create.ts
+++ src/server/controllers/person/Create.ts
@@ -1,12 +1,41 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 interface IPerson {
   name: string;
   email: string;
 }
 
-export const create = (req: Request<{}, {}, IPerson>, res: Response) => {
+const bodyValidation: yup.SchemaOf<IPerson> = yup.object().shape({
+  name: yup.string().min(4).required(),
+  email: yup.string().email().required(),
+});
+
+export const create = async (req: Request<{}, {}, IPerson>, res: Response) => {
   const body = req.body;
+
+  try {
+    const validatedData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
+    });
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+
+    const validationErrors: Record<string, string> = {};
+
+    yupError.inner.forEach((error) => {
+      if (error.path === undefined) {
+        return;
+      }
+
+      validationErrors[error.path] = error.message;
+
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        errors: validationErrors,
+      });
+    });
+  }
 
   return res.send({ message: "Person created!" });
 };
